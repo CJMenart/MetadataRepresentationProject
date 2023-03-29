f9aef30c571eaffcbe4b0f91016172c16ccdcde8
unknown <cjmenart@gmail.com>
2023-03-29 11:03:52 -0400

    Draft add_scenario_markup()

--- materialize.py
+++ materialize.py
@@ -48,25 +48,11 @@ def main(markup_file, cityscapes_root):
     markup = tabs_to_nest(markup)
     
     for scenario in markup:
-        add_scenario(graph, scenario)
-    
+        add_scenario_markup(graph, scenario)
+        add_scenario_json(graph, scenario[0], cityscapes_root)
 
     TODO TODO TODO
     
-    # Initialize from a file
-    # filename = "path/to/file"
-    # with open(filename, "w") as f:
-    #     graph.parse(f)
-
-    # TODO delort
-    kastle_members = ["Cogan", "Andrea", "Brandon"]
-    for x in kastle_members:
-        # Add a specific triple
-        # g.add( (subject_node, predicate_node, object_node) )
-        graph.add( (pfs["ex"][x], a, pfs["ex"]["Person"]) )
-
-
-
 
     # Save
     output_file = "output.ttl"
@@ -86,7 +72,8 @@ def tabs_to_nest(textlines):
     return overall
     
     #TODO add roads/lanes/whatever to scenario too
-def add_scenario(graph, scenario):
+def add_scenario_markup(graph, scenario):
+    posInd = 0
     imname = scenario[0][:scenario[0].index('leftImg8bit.png')-1]
     graph.add((pfs['ex'][imname], a, pfs['ex']['Scenario']))
     for road_or_intr in scenario[1]:
@@ -94,19 +81,52 @@ def add_scenario(graph, scenario):
             # Road
             roadname = f"{imname}_{road_or_intr[0].strip()}"
             graph.add((pfs['ex'][roadname], a, pfs['ex']["Road"]))
+            prevlanename = None
             for lane in road_or_intr[1]:
                 lanename = f"{roadname}_{lane[0].strip()}"
                 graph.add((pfs['ex'][lanename], a, pfs['ex']["Lane"]))
                 graph.add((pfs['ex'][lanename], pfs['ex']['inRoad'], pfs['ex'][roadname]))
+                
+                # Tie lanes together 
+                if prevlanename:
+                    graph.add((pfs['ex'][lanename], pfs['ex']['directlyRightOf'], pfs['ex'][prevlanename]))
+                prevlanename = lanename
+                
+                for entity in lane[1]:
+                    # only records position rn. Type comes from other stuff
+                    e_num = entity[0].lstrip().split()[0]
+                    ename = f"{imname}_{e_num}"
+                    graph.add((pfs['ex'][ename], pfs['ex']['hasPosition'], pfs['ex'][f'{imname}_pos{posInd}']))
+                    graph.add((pfs['ex'][f'{imname}_pos{posInd}'], a, pfs['ex']['Position']))
+                    graph.add((pfs['ex'][f'{imname}_pos{posInd}'], pfs['ex']['hasRelativity', pfs['ex'][f'{imname}_rel{posInd}']))
+                    graph.add((pfs['ex'][f'{imname}_rel{posInd}'], pfs['ex']['relToLane'], pfs['ex'][lanename]))
+                    graph.add((pfs['ex'][f'{imname}_rel{posInd}'], pfs['ex']['relativity'], pfs['ex']['On']))
+                    posInd += 1
         else:
             # Intersections
             intname = f"{imname}_{road_or_intr[0].strip()}"
             graph.add((pfs['ex'][intname], a, pfs['ex']['Intersection']))
-            
+            if 'Imaginary' in intname:
+                graph.add((pfs['ex'][intname], a, pfs['ex']['ImaginaryIntersection']))
+                for cardinality in road_or_intr[1]:
+                    cardiname = cardinality[0].strip()
+                    for direction in cardinality[1]:
+                        dirname = direction[0].strip()
+                        for lane in direction[1]:
+                            letters = lane[0].strip()
+                            lanename = f"{imname}_Road{letters[0]}_Lane{letters[1]}"
+                            touchname = TODO 
+                            graph.add((pfs['ex'][touchname], a, pfs['ex']['TouchingIntersection']))
+                            graph.add((pfs['ex'][intname], pfs['ex']['touchesLane'], pfs['ex'][touchname]))
+                            graph.add((pfs['ex'][lanename], pfs['ex']['touchesIntersection'], pfs['ex'][touchname]))
+                            graph.add((pfs['ex'][touchname], pfs['ex']['hasCardinality'], pfs['ex'][cardiname]))
+                            graph.add((pfs['ex'][touchname], pfs['ex']['hasDirection'], pfs['ex'][dirname]))            
     
     #TODO scrape other objects from Cityscape--filter out those already in?
     
     
-    # TODO tie lanes together 
-    
     # TODO apply Movement of moving vehicles
+    
+    
+add_scenario_json(graph, imname, cityscapes_root):
+    raise NotImplementedError
