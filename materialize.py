b4a3401542edfe460940089f8f8151ef650183d8
unknown <cjmenart@gmail.com>
2023-03-30 09:21:07 -0400

    incomplete materialization.py runs

--- materialize.py
+++ materialize.py
@@ -2,6 +2,7 @@
 import json
 import numpy as np
 from pathlib import Path
+import os, sys
 ##### Graph stuff
 import rdflib
 from rdflib import URIRef, Graph, Namespace, Literal
@@ -22,7 +23,7 @@ pfs = {
 "ssn": Namespace("http://www.w3.org/ns/ssn/"),
 "sosa": Namespace("http://www.w3.org/ns/sosa/"),
 "cdt": Namespace("http://w3id.org/lindt/custom_datatypes#"),
-"ex": Namespace("https://example.com/"),
+"ex": Namespace("http://www.semanticweb.org/rochelle/ontologies/2023/2/untitled-ontology-5#"),  # mod this to actual domain
 "rdf": RDF,
 "rdfs": RDFS,
 "xsd": XSD,
@@ -38,10 +39,11 @@ def init_kg(prefixes=pfs):
 # rdf:type shortcut
 a = pfs["rdf"]["type"]
 
+def no_whitespace(line):
+    return "".join(line.split())
+
 
 def main(markup_file, cityscapes_root):
-
-
     # Initialize an empty graph
     graph = init_kg()
 
@@ -53,6 +55,7 @@ def main(markup_file, cityscapes_root):
     
     # TODO scenario-level metadata
     for scenario in markup:
+        print(f"main imname: {scenario[0]}")
         add_scenario_markup(graph, scenario)
         add_scenario_json(graph, scenario[0], cityscapes_root)
 
@@ -60,20 +63,24 @@ def main(markup_file, cityscapes_root):
     
 
     # Save
-    output_file = "output.ttl"
+    output_file = "materialization.ttl"
     temp = graph.serialize(format="turtle", encoding="utf-8", destination=output_file)
+    print("Done!")
     
     
 def tabs_to_nest(textlines):
     overall=[]
     
     for line in textlines:
+        line = line.rstrip()
         ntabs = line.count('\t')
+        line = line.lstrip()
+        print(f"ntabs, line: {ntabs}, {line}")
         dest = overall
         for t in range(ntabs):
             dest = dest[-1][1]
-        if not line.isspace():
-            dest.append((line.strip(), []))
+        if line and not line.isspace():
+            dest.append((line, []))
     return overall
     
 
@@ -85,11 +92,11 @@ def add_scenario_markup(graph, scenario):
     for road_or_intr in scenario[1]:
         if 'Road' in road_or_intr:
             # Road
-            roadname = f"{imname}_{road_or_intr[0].strip()}"
+            roadname = f"{imname}_{no_whitespace(road_or_intr[0])}"
             graph.add((pfs['ex'][roadname], a, pfs['ex']["Road"]))
             prevlanename = None
             for lane in road_or_intr[1]:
-                lanename = f"{roadname}_{lane[0].strip()}"
+                lanename = f"{roadname}_{no_whitespace(lane[0])}"
                 graph.add((pfs['ex'][lanename], a, pfs['ex']["Lane"]))
                 graph.add((pfs['ex'][lanename], pfs['ex']['inRoad'], pfs['ex'][roadname]))
                 
@@ -102,15 +109,18 @@ def add_scenario_markup(graph, scenario):
                     # only records position rn. Type comes from other stuff
                     e_num = entity[0].lstrip().split()[0]
                     ename = f"{imname}_{e_num}"
+                    if 'Self' in ename:
+                        graph.add((pfs['ex'][ename], a, pfs['ex']['Self']))
                     graph.add((pfs['ex'][ename], pfs['ex']['hasPosition'], pfs['ex'][f'{imname}_pos{posInd}']))
                     graph.add((pfs['ex'][f'{imname}_pos{posInd}'], a, pfs['ex']['Position']))
-                    graph.add((pfs['ex'][f'{imname}_pos{posInd}'], pfs['ex']['hasRelativity', pfs['ex'][f'{imname}_rel{posInd}']))
+                    graph.add((pfs['ex'][f'{imname}_pos{posInd}'], pfs['ex']['hasRelativity'], pfs['ex'][f'{imname}_rel{posInd}']))
                     graph.add((pfs['ex'][f'{imname}_rel{posInd}'], pfs['ex']['relToLane'], pfs['ex'][lanename]))
                     graph.add((pfs['ex'][f'{imname}_rel{posInd}'], pfs['ex']['relativity'], pfs['ex']['On']))
                     posInd += 1
         else:
             # Intersections
-            intname = f"{imname}_{road_or_intr[0].strip()}"
+            intname = f"{imname}_{no_whitespace(road_or_intr[0])}"
+            print(f"intname: {intname}")
             graph.add((pfs['ex'][intname], a, pfs['ex']['Intersection']))
             if 'Imaginary' in intname:
                 graph.add((pfs['ex'][intname], a, pfs['ex']['ImaginaryIntersection']))
@@ -121,7 +131,7 @@ def add_scenario_markup(graph, scenario):
                         for lane in direction[1]:
                             letters = lane[0].strip()
                             lanename = f"{imname}_Road{letters[0]}_Lane{letters[1]}"
-                            touchname = TODO 
+                            touchname = f"{imname}_{letters}2{intname[len('Intersection'):]}"
                             graph.add((pfs['ex'][touchname], a, pfs['ex']['TouchingIntersection']))
                             graph.add((pfs['ex'][intname], pfs['ex']['touchesLane'], pfs['ex'][touchname]))
                             graph.add((pfs['ex'][lanename], pfs['ex']['touchesIntersection'], pfs['ex'][touchname]))
@@ -138,20 +148,21 @@ def add_scenario_markup(graph, scenario):
 def add_scenario_json(graph, imname, cityscapes_root):
     cityscapes_root = Path(cityscapes_root)
     split = 'train'  # TODO detect this if more data is added 
-    city = scenario[0].split('_')[0]
-    imname = scenario[0][:scenario[0].index('leftImg8bit.png')-1]
+    city = imname.split('_')[0]
+    imname = imname[:imname.index('leftImg8bit.png')-1]
+    print(f"add_scenario_json imname: {imname}")
     
-    bbox3d_json = cityscapes_root / 'gtBbox3d' / split / imname + '_gtBbox3d.json'
+    bbox3d_json = cityscapes_root / 'gtBbox3d' / split / city / (imname + '_gtBbox3d.json')
     with open(bbox3d_json, 'r') as f:
         bbox3d = json.load(f)
     add_vehicles(graph, imname, bbox3d)
     
-    people_json = cityscapes_root / 'gtBboxCityPersons' / split / imname + '_gtBboxCityPersons.json'
+    people_json = cityscapes_root / 'gtBboxCityPersons' / split / city / (imname + '_gtBboxCityPersons.json')
     with open(people_json, 'r') as f:
         people = json.load(f)
     add_pedestrian(graph, imname, people)
     
-    semseg_json = cityscapes_root / 'gtFine' / split / imname + '_gtFine_polygons.json'
+    semseg_json = cityscapes_root / 'gtFine' / split / city / (imname + '_gtFine_polygons.json')
     with open(semseg_json, 'r') as f:
         misc = json.load(f)
     add_misc(graph, imname, misc)
@@ -161,7 +172,7 @@ def add_vehicles(graph, imname, bbox3d):
     use_classes = ['car', 'truck', 'bus', 'train', 'motorcycle', 'bicycle', 'caravan']
     # ignore: tunnel, dynamic, trailer
         
-    objects = bbbox3d['objects']
+    objects = bbox3d['objects']
     
     
     for obj in objects:
@@ -214,3 +225,23 @@ def add_misc(graph, imname, instance_labels):
             misc_id += 1
             graph.add((pfs['ex'][f"{imname}_{misc_id}"], a, pfs['ex']['PhysicalThing']))
             graph.add((pfs['ex'][imname], pfs['ex']['hasThing'], pfs['ex'][f"{imname}_{misc_id}"]))
+            
+
+"""
+def parse_scenario_metadata(graph, imname, metadata):
+    metadata['speed']
+    metadata['outsideTemperature']
+    
+    
+    {
+    "gpsHeading": 63, 
+    "gpsLatitude": 51.476743661537675, 
+    "gpsLongitude": 7.192954591463274, 
+    "outsideTemperature": 22.5, 
+    "speed": 8.41225, 
+    "yawRate": -0.059063487031132955
+}
+"""
+
+if __name__=='__main__':
+    main(sys.argv[1], sys.argv[2])
